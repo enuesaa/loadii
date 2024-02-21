3977f180660d8f63f41379f914819a4892d5dac9
nua <enuesaa@gmail.com>
2024-02-22 00:43:02 +0900

    feat: go mod init

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "tryserve",
+		Version: "0.0.1",
+		Usage: "Instance web server",
+		Action: func(c *cli.Context) error {
+			fmt.Println("hello")
+			return nil
+		},
+	}
+
+	// disable default
+	app.HideHelpCommand = true
+	cli.AppHelpTemplate = `{{.Usage}}
+
+USAGE:
+	{{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
+	{{if len .Authors}}
+AUTHOR:
+	{{range .Authors}}{{ . }}{{end}}
+	{{end}}{{if .Commands}}
+COMMANDS:
+{{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+GLOBAL OPTIONS:
+	{{range .VisibleFlags}}{{.}}
+	{{end}}{{end}}
+`
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
