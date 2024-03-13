c9197b7bb5a6427486e8df4d188e4f95b51ff89a
nua <enuesaa@gmail.com>
2024-03-14 00:31:10 +0900

    fix(run): validate argument

--- pkg/command/run.go
+++ pkg/command/run.go
@@ -1,6 +1,8 @@
 package command
 
 import (
+	"fmt"
+
 	"github.com/enuesaa/loadii/pkg/repository"
 	"github.com/enuesaa/loadii/pkg/usecase"
 	"github.com/urfave/cli/v2"
@@ -8,12 +10,14 @@ import (
 
 func NewRunCommand(repos repository.Repos, watchpath *string) *cli.Command {
 	cmd := cli.Command{
-		Name:  "run",
-		Usage: "run app. Currently, this command supports golang app.",
+		Name:      "run",
+		Usage:     "run app. Currently, this command supports golang app.",
+		Args:      true,
+		ArgsUsage: "filename",
 		Before: func(c *cli.Context) error {
 			path := c.Args().Get(0)
 			if path == "" {
-				return cli.ShowAppHelp(c)
+				return fmt.Errorf("please pass filename to run")
 			}
 			return nil
 		},
