ca991a0909cbf843c3f3ce1367a3882f5d108c6b
nua <enuesaa@gmail.com>
2024-03-17 16:42:37 +0900

    feat: remove subcommands and define flags

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/enuesaa/loadii/pkg/command"
 	"github.com/enuesaa/loadii/pkg/repository"
 	"github.com/enuesaa/loadii/pkg/usecase"
 	"github.com/urfave/cli/v2"
@@ -14,24 +13,51 @@ func main() {
 	repos := repository.New()
 
 	var watchpath string
+	var watchExcludePath string
+	var workdir string
+	var servepath string
+	var serveport int
 
 	app := &cli.App{
 		Name:    "loadii",
 		Version: "0.0.3",
-		Usage:   "A CLI tool to watch file changes and execute a task",
-		Commands: []*cli.Command{
-			command.NewExecCommand(repos, &watchpath),
-			command.NewServeCommand(repos, &watchpath),
-		},
+		Usage:   "A CLI tool to watch file changes and execute a command",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:        "watch",
-				Aliases:     []string{"w"},
-				Usage:       "watch dir",
-				Value:       ".",
+				Name:        "include",
+				Usage:       "add path to watch",
+				Value:       "",
 				Destination: &watchpath,
 			},
+			&cli.StringFlag{
+				Name:        "exclude",
+				Usage:       "remove path to watch",
+				Value:       "",
+				Destination: &watchExcludePath,
+			},
+			&cli.StringFlag{
+				Name:        "serve",
+				Usage:       "serve",
+				Value:       "",
+				Destination: &servepath,
+			},
+			&cli.IntFlag{
+				Name: "port",
+				Aliases: []string{"p"},
+				Usage: "port",
+				Value: 3000,
+				Destination: &serveport,
+			},
+			&cli.StringFlag{
+				Name: "workdir",
+				Aliases: []string{"w"},
+				Usage: "workdir",
+				Value: ".",
+				Destination: &workdir,
+			},
 		},
+		Args:      true,
+		ArgsUsage: "commands",
 		Action: func(c *cli.Context) error {
 			if len(c.FlagNames()) == 0 {
 				return cli.ShowAppHelp(c)
