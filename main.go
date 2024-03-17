95462406e9b1e617d563f97c734bebd955c87439
nua <enuesaa@gmail.com>
2024-03-17 17:22:49 +0900

    feat(cli): grouping flags

--- main.go
+++ main.go
@@ -1,69 +1,87 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
-	"github.com/enuesaa/loadii/pkg/repository"
-	"github.com/enuesaa/loadii/pkg/usecase"
+	// "github.com/enuesaa/loadii/pkg/repository"
+	// "github.com/enuesaa/loadii/pkg/usecase"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	repos := repository.New()
+	// repos := repository.New()
 
-	var watchpath string
-	var watchExcludePath string
+	var watchIncludes cli.StringSlice
+	var watchExcludes cli.StringSlice
 	var workdir string
-	var servepath string
-	var serveport int
+	var servePath string
+	var servePort int
+	var autoApprove bool
 
 	app := &cli.App{
 		Name:    "loadii",
 		Version: "0.0.3",
 		Usage:   "A CLI tool to watch file changes and execute a command",
 		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:        "include",
-				Usage:       "add path to watch",
-				Value:       "",
-				Destination: &watchpath,
+			&cli.BoolFlag{
+				Name:        "yes",
+				Aliases:     []string{"y"},
+				Value:       false,
+				Usage:       "Auto approve",
+				Destination: &autoApprove,
 			},
-			&cli.StringFlag{
+			&cli.StringSliceFlag{
+				Name:        "include",
+				Usage:       "Add path to watch",
+				Value: cli.NewStringSlice("."),
+				Destination: &watchIncludes,
+				Category: "watch",
+			},
+			&cli.StringSliceFlag{
 				Name:        "exclude",
-				Usage:       "remove path to watch",
-				Value:       "",
-				Destination: &watchExcludePath,
+				Usage:       "Remove path to watch",
+				Destination: &watchExcludes,
+				Category: "watch",
 			},
 			&cli.StringFlag{
 				Name:        "serve",
-				Usage:       "serve",
-				Value:       "",
-				Destination: &servepath,
+				Usage:       "Serve dir",
+				Destination: &servePath,
+				Category: "serve",
 			},
 			&cli.IntFlag{
 				Name: "port",
-				Aliases: []string{"p"},
-				Usage: "port",
+				Usage: "Serve port",
 				Value: 3000,
-				Destination: &serveport,
+				Destination: &servePort,
+				Category: "serve",
 			},
 			&cli.StringFlag{
 				Name: "workdir",
 				Aliases: []string{"w"},
-				Usage: "workdir",
+				Usage: "Command execution dir",
 				Value: ".",
 				Destination: &workdir,
+				Category: "serve",
 			},
 		},
 		Args:      true,
 		ArgsUsage: "commands",
 		Action: func(c *cli.Context) error {
+			fmt.Printf("includes: %+v\n", watchIncludes.Value())
+			fmt.Printf("excludes: %+v\n", watchExcludes.Value())
+			fmt.Printf("workdir: %+v\n", workdir)
+			fmt.Printf("serve: %+v\n", servePath)
+			fmt.Printf("port: %+v\n", servePort)
+			fmt.Printf("autoApprove: %+v\n", autoApprove)
+		
 			if len(c.FlagNames()) == 0 {
 				return cli.ShowAppHelp(c)
 			}
 
-			usecase.Watch(repos, watchpath)
+			// usecase.Watch(repos, ".")
 
 			return nil
 		},
@@ -78,15 +96,16 @@ func main() {
 	cli.AppHelpTemplate = `{{.Usage}}
 
 USAGE:
-	{{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
+	{{.HelpName}} {{if .VisibleFlags}}[flags]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}
 	{{if len .Authors}}
 AUTHOR:
 	{{range .Authors}}{{ . }}{{end}}
 	{{end}}{{if .Commands}}
 COMMANDS:
 {{range .Commands}}{{if not .HideHelp}}   {{join .Names ", "}}{{ "\t"}}{{.Usage}}{{ "\n" }}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
-GLOBAL OPTIONS:
-	{{range .VisibleFlags}}{{.}}
+FLAGS:{{range .VisibleFlagCategories}}{{if len .Name}}[{{.Name}}]{{end}}
+	{{range .Flags}}{{.}}
+	{{end}}
 	{{end}}{{end}}
 `
 
