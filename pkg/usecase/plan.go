825e22c00c0b421a05d87060cbd1fc34d89f03cc
nua <enuesaa@gmail.com>
2024-03-30 14:43:26 +0900

    feat(plan): do not print not configured action planning

--- pkg/usecase/plan.go
+++ pkg/usecase/plan.go
@@ -10,20 +10,15 @@ type PlanProps struct {
 	WatchExcludes []string
 }
 func Plan(repos repository.Repos, props PlanProps) {
-	// TODO: this is prototype
 	// print planning like fiber v2 message
 	repos.Log.Info("┌──────────────────────")
 	repos.Log.Info("│ Loadii")
 	repos.Log.Info("│")
 	if props.ServePath != "" {
 		repos.Log.Info("│ Serve:   %s on %d", props.ServePath, props.ServePort)
-	} else {
-		repos.Log.Info("│ Serve:   (not configured)")
 	}
 	if len(props.Commands) > 0 {
 		repos.Log.Info("│ Command: %+v", props.Commands)
-	} else {
-		repos.Log.Info("│ Command: (not configured)")
 	}
 	repos.Log.Info("│ Watch:   includes: %+v, excludes: %+v", props.WatchIncludes, props.WatchExcludes)
 	repos.Log.Info("└──────────────────────")
