264c9a64147467afc3df1cf697124bc77e36fcb8
nua <enuesaa@gmail.com>
2024-03-30 20:02:47 +0900

    fix(log): log contains un-need empty line

--- pkg/exec/writer.go
+++ pkg/exec/writer.go
@@ -11,10 +11,14 @@ type CmdWriter struct {
 }
 
 // implements io.Writer
+// TODO refactor. should stdout with colored output.
 func (w *CmdWriter) Write(p []byte) (n int, err error) {
 	text := string(p)
 	texts := strings.Split(text, "\n")
 
+	if texts[len(texts) - 1] == "" {
+		texts = texts[:len(texts)-1]
+	}
 	for _, line := range texts {
 		w.repos.Log.Info(line)
 	}
