64663b5ff3ab74edc0a5c15e62f4bfab22b6e805
nua <enuesaa@gmail.com>
2024-04-05 01:28:34 +0900

    chore(repos): remove unused methods.

--- pkg/repository/fs.go
+++ pkg/repository/fs.go
@@ -10,14 +10,8 @@ type FsRepositoryInterface interface {
 	Ext(path string) string
 	IsExist(path string) bool
 	IsDir(path string) (bool, error)
-	CreateDir(path string) error
-	Create(path string, body []byte) error
-	HomeDir() (string, error)
 	WorkDir() (string, error)
-	Remove(path string) error
 	Read(path string) ([]byte, error)
-	ListDirs(path string) ([]string, error)
-	ListFiles(path string) ([]string, error)
 }
 type FsRepository struct{}
 
@@ -40,32 +34,8 @@ func (repo *FsRepository) IsDir(path string) (bool, error) {
 	return f.IsDir(), nil
 }
 
-func (repo *FsRepository) CreateDir(path string) error {
-	return os.MkdirAll(path, os.ModePerm)
-}
-
-func (repo *FsRepository) Create(path string, body []byte) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	if _, err := file.Write(body); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (repo *FsRepository) HomeDir() (string, error) {
-	return os.UserHomeDir()
-}
-
 func (repo *FsRepository) WorkDir() (string, error) {
 	return os.Getwd()
-}
-
-func (repo *FsRepository) Remove(path string) error {
-	return os.RemoveAll(path)
 }
 
 func (repo *FsRepository) Read(path string) ([]byte, error) {
@@ -75,33 +45,4 @@ func (repo *FsRepository) Read(path string) ([]byte, error) {
 	}
 	defer f.Close()
 	return io.ReadAll(f)
-}
-
-func (repo *FsRepository) ListDirs(path string) ([]string, error) {
-	list := make([]string, 0)
-	err := filepath.Walk(path, func(fpath string, file os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if file.IsDir() {
-			list = append(list, fpath)
-		}
-		return nil
-	})
-	return list, err
-}
-
-func (repo *FsRepository) ListFiles(path string) ([]string, error) {
-	list := make([]string, 0)
-	err := filepath.Walk(path, func(fpath string, file os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if file.IsDir() {
-			return nil
-		}
-		list = append(list, fpath)
-		return nil
-	})
-	return list, err
 }
